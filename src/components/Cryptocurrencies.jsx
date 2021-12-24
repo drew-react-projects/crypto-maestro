31a87ef6d730214590a9cb8d161f635607cb6b59
Andrew Le <s3820098@rmit.edu.vn>
2021-12-24 12:15:14 +0700

    15. Implement search bar for all cryptocurrencies page

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input, Typography } from 'antd';
@@ -10,7 +10,15 @@ const { Title } = Typography;
 const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 50;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const filterredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setCryptos(filterredData);
+  }, [cryptosList, searchTerm]); // do something when these values change
 
   if (isFetching) return 'Loading...';
 
@@ -21,6 +29,16 @@ const Cryptocurrencies = ({ simplified }) => {
           All 50 Cryptocurrencies
         </Title>
       )}
+
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
+
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
