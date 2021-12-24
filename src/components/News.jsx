507cb380025df36028a6ee9ad133f74b44500640
Andrew Le <s3820098@rmit.edu.vn>
2021-12-24 15:32:40 +0700

    17. Render news cards and  their content

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,15 +7,70 @@ import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 const { Text, Title } = Typography;
 const { Option } = Select;
 
+const demoImage =
+  'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+
 const News = ({ simplified }) => {
   const { data: cryptoNews } = useGetCryptoNewsQuery({
     newsCategory: 'Cryptocurrency',
-    count: simplified ? 10 : 50,
+    count: simplified ? 6 : 18,
   });
 
-  console.log(cryptoNews);
+  if (!cryptoNews?.value) return 'Loading...';
 
-  return <div></div>;
+  return (
+    <>
+      {!simplified && (
+        <Title level={2} className="heading">
+          Lastest Crypto News
+        </Title>
+      )}
+      <Row gutter={[24, 24]}>
+        {cryptoNews.value.map((news, i) => (
+          <Col xs={24} sm={12} lg={8} key={i}>
+            <Card hoverable className="news-card">
+              <a href={news.url} target="_blank" rel="noreferrer">
+                <div className="news-image-container">
+                  <Title className="news-title" level={4}>
+                    {news.name}
+                  </Title>
+                  <img
+                    src={news?.image?.thumbnail?.contentUrl || demoImage}
+                    alt="news card thumbnail"
+                  />
+                </div>
+
+                <p>
+                  {news.description.length > 100
+                    ? `${news.description.substring(0, 120)} ...`
+                    : news.description}
+                </p>
+
+                {/* news provider section */}
+                <div className="provider-container">
+                  <div>
+                    <Avatar
+                      src={
+                        news.provider[0]?.image?.thumbnail?.contentUrl ||
+                        demoImage
+                      }
+                      alt="provider logo"
+                    />
+                    <Text className="provider-name">
+                      {news.provider[0].name}
+                    </Text>
+                  </div>
+                  <Text>
+                    {moment(news.datePublished).startOf('ss').fromNow()}
+                  </Text>
+                </div>
+              </a>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default News;
