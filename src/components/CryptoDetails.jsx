dfca67d0e8e8efef896b04d81e82c31eacb196be
Andrew Le <s3820098@rmit.edu.vn>
2021-12-29 18:52:21 +0700

    19. Necessary imports + show coin id using use param

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import HTMLReactParser from 'html-react-parser';
+import { useParams } from 'react-router-dom';
+import millify from 'millify';
+import { Col, Row, Typography, Select } from 'antd';
+import {
+  MoneyCollectOutlined,
+  DollarCircleOutlined,
+  FundOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  TrophyOutlined,
+  CheckOutlined,
+  NumberOutlined,
+  ThunderboltOutlined,
+} from '@ant-design/icons';
+
+const { Title, Text } = Typography;
+const { Option } = Select;
 
 const CryptoDetails = () => {
-  return <div></div>;
+  const { coinId } = useParams();
+  const [timeperiod, setTimeperiod] = useState('7d');
+
+  return <div>{ coinId }</div>;
 };
 
 export default CryptoDetails;
